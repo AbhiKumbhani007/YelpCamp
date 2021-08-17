5260e851115ada4ed01a29bb7a8bc8d41966afb8
AbhiKumbhani007 <abhi1234kumbhani@gmail.com>
2021-08-17 17:47:01 +0530

    Injected all template's data in boilerplates

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 const Campground = require("./models/campground")
 const methodOverride = require("method-override")
-
+const ejsMate = require('ejs-mate')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useNewUrlParser: true,
@@ -23,6 +23,8 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded( { extended: true } ));
 app.use(express.json());
 app.use(methodOverride('_method'))
+app.engine('ejs', ejsMate)
+
 
 app.get("/", (req, res) => {
 	res.render("home")
