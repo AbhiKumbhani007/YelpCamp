5da188dc34f17bd382500c0553456d270673ad07
AbhiKumbhani007 <abhi1234kumbhani@gmail.com>
2021-09-10 09:56:36 +0530

    user controller added

--- controller/user.js
+++ controller/user.js
@@ -1,16 +1,10 @@
-const express = require('express')
-const router = express.Router()
 const User = require('../models/user')
-const catchAsync = require('../utils/catchAsync')
 const passport = require('passport')
 const localStratagy = require('passport-local')
-const userController = require('../controller/user')
-
-router.get('/register', (req, res) => {
+module.exports.renderRegisterForm = (req, res) => {
 	res.render('users/register')
-})
-
-router.post('/register', catchAsync(async (req, res, next) => {
+}
+module.exports.registerUser = async (req, res, next) => {
 	try {
 		const { email, username, password } = req.body
 		const user = new User({ email, username })
@@ -29,23 +23,18 @@ router.post('/register', catchAsync(async (req, res, next) => {
 		res.redirect('/register')
 	}
 
-}))
-
-router.get('/login', (req, res) => {
+}
+module.exports.renderLoginForm = (req, res) => {
 	res.render('users/login')
-})
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: 'login' }), (req, res) => {
+}
+module.exports.loginUser = passport.authenticate('local', { failureFlash: true, failureRedirect: 'login' }), (req, res) => {
 	req.flash('success', 'Welcome back')
 	const redirectUrl = req.session.returnTo || '/campgrounds'
 	delete req.session.returnTo
 	res.redirect(redirectUrl)
-})
-
-router.get('/logout', (req, res) => {
+}
+module.exports.logoutUser = (req, res) => {
 	req.logOut()
 	req.flash('success', 'Logged You Out')
 	res.redirect('/campgrounds')
-})
-
-module.exports = router
+}
